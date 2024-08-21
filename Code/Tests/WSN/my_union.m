68078bae30db3226d8c159f8a122254d350d6b0a
uneeb125 <uneebkamal125@gmail.com>
2024-08-21 09:40:54 -0700

    changed the npolygon from 1000 to 100 for some speed advantage at cost of accuracy

--- Code/Tests/WSN/my_union.m
+++ Code/Tests/WSN/my_union.m
@@ -1,9 +1,9 @@
-function coveredarea = my_area(nodes,Rsensors)
+function coveredarea = my_union(nodes,Rsensors)
 nNodes = length(nodes)/2;
 x = 1:2:nNodes*2;
 y = x+1;
 for i=1:nNodes 
-    SensorArray(i) = nsidedpoly(1000,'Center',[nodes(x(i)) nodes(y(i))], 'Radius', Rsensors);
+    SensorArray(i) = nsidedpoly(100,'Center',[nodes(x(i)) nodes(y(i))], 'Radius', Rsensors);
 end
 
 coveredarea = area(union(SensorArray));
